cc198b014e580fc9e5844e4a9a5af7abda65f2fb
Pavit Kaur <63570991+pavit15@users.noreply.github.com>
2023-09-20 15:06:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -22,8 +22,6 @@ function displayContactInfo() {
 }
 
 // Google Map API
-
-
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 19.0760, lng: 72.8777}, // Change the coordinates to your desired location
